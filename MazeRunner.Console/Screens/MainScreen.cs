a673b4dc89c042fd0acec3c5b37b9bf9628b4603
Klein Reveche <therealklr2003@gmail.com>
2023-10-31 19:06:11 +0800

    feat(maze-runner-console): add how to play screen

--- MazeRunner.Console/Screens/MainScreen.cs
+++ MazeRunner.Console/Screens/MainScreen.cs
@@ -24,7 +24,48 @@ public static class MainScreen
                                   ╚═╝░░╚═╝░╚═════╝░╚═╝░░╚══╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝
                                   """;
 
-    public static readonly int CenterX = (System.Console.WindowWidth - Runner.Split('\n')[0].Length) / 2;
+    private const string About = """
+                                 Credits
+                                 --------
+                                 Created by Klein Reveche.
+                                 Version 1.0.0
+
+                                 Music
+                                 --------
+                                 8-bit Air Fight by moodmode from Pixabay
+                                 Bit Beats 3 by XtremeFreddy from Pixabay
+                                 Other sound effects from Pixabay
+
+                                 © 2023 Klein Reveche. All rights reserved.
+                                 """;
+    
+    private const string HowToPlay = """
+                                      How to Play
+                                      -----------
+                                      Use the arrow keys to move the runner.
+                                      Go to the door to advance to the next level.
+                                      Avoid the enemies.
+
+                                      Controls:
+                                      - Arrow Keys or WASD Keys to move player.
+                                      - Place a bomb with the B Key. It will detonate after 2 turns.
+                                      - Place a candle with the C Key. It has a 3x3 range and will last for the current level.
+                                      - Note: The bomb and candle will be placed on the last position of the player.
+
+                                      Game Modes:
+                                      - Classic: Play through 6 levels of increasing difficulty.
+                                      - Endless: Play through endless levels of increasing difficulty.
+
+                                      Difficulty:
+                                      - Easy: 4 levels on Classic, High visibility, more starting items.
+                                      - Normal: 5 levels on Classic, Normal visibility, normal starting items.
+                                      - Hard: 5 levels on Classic, Reduced visibility, less starting items.
+                                      - Insanity: 6 levels on Classic, Almost no visibility, least starting items.
+                                      - ASCII Insanity: Same with Insanity but a forced ASCII Look.
+                                      """;
+
+    private static readonly string Separator = OperatingSystem.IsWindows() ? "\r\n" : "\n";
+    public static readonly int CenterX = (System.Console.WindowWidth - Runner.Split(Separator)[0].Length) / 2;
 
     internal static GameState GameState = OptionsManager.LoadOptions();
     private static ClassicState _classicState = new();
@@ -35,13 +76,13 @@ public static class MainScreen
     {
         var buffer = new StringBuilder();
         System.Console.ForegroundColor = ConsoleColor.White;
-        foreach (var line in Maze.Split('\n'))
+        foreach (var line in Maze.Split(Separator))
         {
             buffer.Append(' ', CenterX + 8);
             buffer.AppendLine(line);
         }
 
-        foreach (var line in Runner.Split('\n'))
+        foreach (var line in Runner.Split(Separator))
         {
             buffer.Append(' ', CenterX);
             buffer.AppendLine(line);
@@ -79,7 +120,8 @@ public static class MainScreen
         Dictionary<string, Action> menuOptions = new()
         {
             { "Continue", () => _gameEngineConsole.Play() },
-            { "Start", () =>
+            { 
+                "Start", () =>
                 {
                     GameState = OptionsManager.LoadOptions();
                     _classicState = new ClassicState();
@@ -94,7 +136,8 @@ public static class MainScreen
                     leaderboardScreen.ShowScreen();
                 }
             },
-            { "Credits", ShowCreditsScreen },
+            { "How to Play", () => ShowTextScreen(HowToPlay, -12) },
+            { "Credits", () => ShowTextScreen(About, 8) },
             { "Quit", () => Environment.Exit(0) }
         };
 
@@ -152,28 +195,14 @@ public static class MainScreen
         }
     }
 
-    private static void ShowCreditsScreen()
+    private static void ShowTextScreen(string text, int padding = 0)
     {
-        const string about = """
-                             Credits
-                             --------
-                             Created by Klein Reveche.
-                             Version 0.5.0
-
-                             Music
-                             --------
-                             8-bit Air Fight by moodmode from Pixabay
-                             Bit Beats 3 by XtremeFreddy from Pixabay
-
-                             © 2023 Klein Reveche. All rights reserved.
-                             """;
-
         System.Console.Clear();
         DisplayTitle();
 
-        foreach (var line in about.Split('\n'))
+        foreach (var line in text.Split(Separator))
         {
-            System.Console.SetCursorPosition(CenterX + 8, System.Console.CursorTop);
+            System.Console.SetCursorPosition(CenterX + padding, System.Console.CursorTop);
             System.Console.WriteLine(line);
         }
 
